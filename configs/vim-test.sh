16d1f0510d29da34598e1b900b6bd1057cec81d8
Nik Visenko <nik.vschenko@sylvera.io>
2022-12-30 18:07:54 +0200

    add comments and add a check for a running shell process before sending keys

--- configs/vim-test.sh
+++ configs/vim-test.sh
@@ -1,31 +1,30 @@
 function vim_test() {
-current_pane_index=$(tmux list-panes -f '#{pane_active}' -F '#{pane_index}')
-is_pane_zoomed=$(tmux list-panes -f '#{pane_active}' -F '#{window_zoomed_flag}')
+  current_pane_index=$(tmux list-panes -f '#{pane_active}' -F '#{pane_index}')
+  is_pane_zoomed=$(tmux list-panes -f '#{pane_active}' -F '#{window_zoomed_flag}')
 
-# if pane is zoomed, we want to zoom out, because zoom affect list-panes command
-if [ $is_pane_zoomed -eq 1 ]; then
-  $(tmux resize-pane -Z)
-fi
+  # if pane is zoomed, we want to zoom out, because zoom affects list-panes command
+  if [ $is_pane_zoomed -eq 1 ]; then
+    $(tmux resize-pane -Z)
+  fi
 
-bottom_pane_index_list=("${(@f)$(tmux list-panes -f '#{pane_at_bottom}' -F '#{pane_index}')}")
-top_pane_index_list=("${(@f)$(tmux list-panes -f '#{pane_at_top}' -F '#{pane_index}')}")
-bottom_pane_count=${#bottom_pane_index_list[@]}
-local test_command="$1"
+  bottom_pane_index_list=("${(@f)$(tmux list-panes -f '#{pane_at_bottom}' -F '#{pane_index}')}")
+  top_pane_index_list=("${(@f)$(tmux list-panes -f '#{pane_at_top}' -F '#{pane_index}')}")
+  bottom_pane_count=${#bottom_pane_index_list[@]}
+  first_bottom_pane_index=${bottom_pane_index_list[@]:0:1}
+  currently_running_process=$(tmux display -p -t bottom '#{pane_current_command}')
+  local test_command="$1"
 
-# will create horizontal split if it doesnt exist
-# exec zsh stops tmux pane from closing when process exists without
-# setting global remain-on-exit tmux option
-if [[ $bottom_pane_index_list == $top_pane_index_list ]]; then
+  # use first existing horizontal split if it exists and is running a shell
+  # process. This avoids sending test command to splits running vim/other
+  # long-running processes
+  if [[ $bottom_pane_count -ge 1 && $currently_running_process =~ (zsh|bash|fish) ]]; then
+    $(tmux send-keys -t bottom "$test_command" Enter)
+    return 0
+  fi
+
+  # else create new horizontal split
+  # exec zsh stops tmux pane from closing when process exists.
+  # This helps to avoid setting global remain-on-exit tmux option
   $(tmux splitw -dv "$test_command && exec zsh")
   return 0
-fi
-
-# use first existing horizontal split 
-if [[ $bottom_pane_count -ge 1 ]]; then
-  $(tmux send-keys -t bottom "$test_command" Enter)
-  return 0
-fi
-
-return 0
-
 }
