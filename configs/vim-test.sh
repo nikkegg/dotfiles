9418ef702c614d37b699fc389c9ed3f24454778c
Nik Visenko <nik.vschenko@sylvera.io>
2022-12-30 17:19:23 +0200

    refactor vim test

--- configs/vim-test.sh
+++ configs/vim-test.sh
@@ -1,43 +1,31 @@
 function vim_test() {
 current_pane_index=$(tmux list-panes -f '#{pane_active}' -F '#{pane_index}')
 is_pane_zoomed=$(tmux list-panes -f '#{pane_active}' -F '#{window_zoomed_flag}')
+
+# if pane is zoomed, we want to zoom out, because zoom affect list-panes command
+if [ $is_pane_zoomed -eq 1 ]; then
+  $(tmux resize-pane -Z)
+fi
+
 bottom_pane_index_list=("${(@f)$(tmux list-panes -f '#{pane_at_bottom}' -F '#{pane_index}')}")
 top_pane_index_list=("${(@f)$(tmux list-panes -f '#{pane_at_top}' -F '#{pane_index}')}")
 bottom_pane_count=${#bottom_pane_index_list[@]}
 local test_command="$1"
 
-# will create horizontal split
+# will create horizontal split if it doesnt exist
 # exec zsh stops tmux pane from closing when process exists without
 # setting global remain-on-exit tmux option
-if [[ $bottom_pane_index_list == $top_pane_index_list && $is_pane_zoomed -ne 1 ]]; then
+if [[ $bottom_pane_index_list == $top_pane_index_list ]]; then
   $(tmux splitw -dv "$test_command && exec zsh")
   return 0
 fi
 
 # use first existing horizontal split 
-if [[ $bottom_pane_count -ge 1 && $is_pane_zoomed -ne 1 ]]; then
+if [[ $bottom_pane_count -ge 1 ]]; then
   $(tmux send-keys -t bottom "$test_command" Enter)
   return 0
-fi
-
-if [ $is_pane_zoomed -eq 1 ]; then
-  $(tmux resize-pane -Z)
-  bottom_pane_index_list_zoomed=("${(@f)$(tmux list-panes -f '#{pane_at_bottom}' -F '#{pane_index}')}")
-  top_pane_index_list_zoomed=("${(@f)$(tmux list-panes -f '#{pane_at_top}' -F '#{pane_index}')}")
-  bottom_pane_count_zoomed=${#bottom_pane_index_list[@]}
-
-  if [[ $bottom_pane_index_list_zoomed == $top_pane_index_list_zoomed ]]; then
-    $(tmux splitw -dv "$test_command && exec zsh")
-    return 0
-  fi
-
-  if [[ $bottom_pane_count_zoomed -ge 1 ]]; then
-    $(tmux send-keys -t bottom "$test_command" Enter)
-    return 0
-  fi
 fi
 
 return 0
 
 }
-
